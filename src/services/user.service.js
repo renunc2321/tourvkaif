1b82805d29b33e15215c3e55eb5b025fb9a04115
cqey <aweelleep@gmail.com>
2023-12-31 12:51:17 +0600

    add postFileUpload

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,27 +1,45 @@
 import axios from 'axios'
 import authHeader from './auth-header'
 
-const API_URL = 'https://kaiftour.catscode.ru/api/admin/statistics'
+const API_URL = 'https://kaiftour.catscode.ru/api'
 
 class UserService {
-  getAllTourHotelData() {
-    return axios
-		.get(API_URL, {
-      headers: {
-				accept: 'application/json',
-				Authorization: authHeader()
-			},
-      params: {
-        isShowOnlyTour: false,
-        isShowOnlyHotel: false,
-        isNewClient: false,
-        page: 1,
-        limit: 10
-      }
-    })
-		.then(response => {
-			return response.data
-		})
+  async getAllTourHotelData() {
+    try {
+      const response = await axios.get(API_URL + '/admin/statistics', {
+        headers: {
+          accept: 'application/json',
+          Authorization: authHeader()
+        },
+        params: {
+          isShowOnlyTour: false,
+          isShowOnlyHotel: false,
+          isNewClient: false,
+          page: 1,
+          limit: 10
+        }
+      })
+
+      return response.data
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  async postFileUpload(id, file) {
+    try {
+      await axios.post(API_URL + '/files/upload', file, {
+        headers: {
+          accept: '*/*',
+          Authorization: authHeader(),
+          'Content-Type': 'multipart/form-data'
+        },
+        params: {
+          paymentId: id
+        }
+      })
+    } catch (error) {
+      console.error(error)
+    }
   }
 }
 
